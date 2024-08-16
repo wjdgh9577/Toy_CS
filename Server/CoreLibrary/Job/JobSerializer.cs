827ab275972e9dda633bf07b482374a48a75693b
Jungho Lee <wjdgh9577@naver.com>
2024-08-17 06:34:33 +0900

    Job 관련 추가

---
+++ Server/CoreLibrary/Job/JobSerializer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLibrary.Job;
+
+public abstract class JobSerializer
+{
+    Queue<IJob> _jobQueue = new Queue<IJob>();
+    object _lock = new object();
+    bool _flush = false;
+
+    public void Push(Action action) { Push(new Job(action)); }
+    public void Push<T1>(Action<T1> action, T1 t1) { Push(new Job<T1>(action, t1)); }
+    public void Push<T1, T2>(Action<T1, T2> action, T1 t1, T2 t2) { Push(new Job<T1, T2>(action, t1, t2)); }
+    public void Push<T1, T2, T3>(Action<T1, T2, T3> action, T1 t1, T2 t2, T3 t3) { Push(new Job<T1, T2, T3>(action, t1, t2, t3)); }
+
+    void Push(IJob job)
+    {
+        lock (_lock)
+        {
+            _jobQueue.Enqueue(job);
+        }
+    }
+
+    protected void Flush()
+    {
+        while (true)
+        {
+            IJob job = Pop();
+            if (job == null)
+                return;
+
+            job.Excute();
+        }
+    }
+
+    IJob Pop()
+    {
+        lock (_lock)
+        {
+            if (_jobQueue.Count == 0)
+            {
+                _flush = false;
+                return null;
+            }
+            return _jobQueue.Dequeue();
+        }
+    }
+
+    public abstract void Update();
+}
---
+++ Server/CoreLibrary/Job/JobTimer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace CoreLibrary.Job;
+
+public class JobTimer
+{
+    public static JobTimer Instance = new JobTimer();
+    JobTimer() { }
+
+    JobTimerPool _pool = new JobTimerPool();
+
+    public void Push(IJob job, int interval, bool autoReset)
+    {
+        var element = _pool.Get();
+
+        element.SetTimer((s, e) =>
+        {
+            job.Excute();
+            _pool.Release(element);
+        }, interval, autoReset);
+
+        element.Start();
+    }
+}
+
+struct JobTimerElement
+{
+    System.Timers.Timer _timer;
+
+    ElapsedEventHandler? _handler;
+
+    public JobTimerElement()
+    {
+        _timer = new System.Timers.Timer();
+        _handler = null;
+        Reset();
+    }
+
+    public void Reset()
+    {
+        _timer.Enabled = false;
+        _timer.Elapsed -= _handler;
+    }
+
+    public void SetTimer(ElapsedEventHandler handler, int interval, bool autoReset)
+    {
+        _handler = handler;
+        _timer.Interval = interval;
+        _timer.AutoReset = autoReset;
+    }
+
+    public void Start()
+    {
+        _timer.Elapsed += _handler;
+        _timer.Enabled = true;
+        _timer.Start();
+    }
+}
+
+class JobTimerPool
+{
+    Queue<JobTimerElement> _timers = new Queue<JobTimerElement>();
+    object _lock = new object();
+
+    public JobTimerElement Get()
+    {
+        JobTimerElement timer;
+
+        lock (_lock)
+        {
+            if (_timers.Count == 0)
+                timer = new JobTimerElement();
+            else
+                timer = _timers.Dequeue();
+        }
+
+        return timer;
+    }
+
+    public void Release(JobTimerElement element)
+    {
+        lock (_lock)
+        {
+            element.Reset();
+            _timers.Enqueue(element);
+        }
+    }
+}
