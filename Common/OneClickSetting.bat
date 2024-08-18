8fa88d2f4b35419018e1e6354c79651eccf283b7
Jungho Lee <wjdgh9577@naver.com>
2024-08-19 06:23:29 +0900

    빌드 자동화

---
+++ Common/OneClickSetting.bat
@@ -0,0 +1,31 @@
+@echo off
+
+cd protoc-27.0-win64
+protoc -I=./ --csharp_out=./ ./Protocol.proto
+IF ERRORLEVEL 1 PAUSE
+
+dotnet build ../../Server/Server.sln --configuration Debug
+IF ERRORLEVEL 1 PAUSE
+
+START /wait ../../Server/CodeGenerator/bin/Debug/net8.0/CodeGenerator.exe ./Protocol.proto
+IF ERRORLEVEL 1 PAUSE
+
+set CLIENT_PLUGIN_PATH="../../Client/Assets/Plugins"
+set SERVER_PLUGIN_PATH="../../Server/TestClient/bin/Debug/net8.0\CoreLibrary.dll"
+set CLIENT_PACKET_PATH="../../Client/Assets/Scripts/Packet"
+set SERVER_PACKET_PATH="../../Server/Server/Packet"
+set DUMMY_PACKET_PATH="../../Server/TestClient/Packet"
+
+XCOPY /Y %SERVER_PLUGIN_PATH% %CLIENT_PLUGIN_PATH%
+
+XCOPY /Y Protocol.cs %CLIENT_PACKET_PATH% >nul
+XCOPY /Y Protocol.cs %SERVER_PACKET_PATH% >nul
+XCOPY /Y Protocol.cs %DUMMY_PACKET_PATH% >nul
+DEL Protocol.cs
+
+XCOPY /Y ClientPacketHandler.cs %CLIENT_PACKET_PATH% >nul
+XCOPY /Y ClientPacketHandler.cs %DUMMY_PACKET_PATH% >nul
+DEL ClientPacketHandler.cs
+
+XCOPY /Y ServerPacketHandler.cs %SERVER_PACKET_PATH% >nul
+DEL ServerPacketHandler.cs
