8bc20839e06c73731e4e3c430ef577a4ae0471b7
Jungho Lee <wjdgh9577@naver.com>
2024-08-20 04:06:09 +0900

    Client LogModule 수정

--- Client/Assets/Scripts/Log/LogModule.cs
+++ Client/Assets/Scripts/Log/LogModule.cs
@@ -5,68 +5,69 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 public sealed class LogModule : LogModuleBase
 {
-    public override void Log(LogType type, LogCode code, params object[] args)
+    public override void Log(CoreLibrary.Log.LogType type, LogCode code, params object[] args)
     {
         if (code == LogCode.CONSOLE)
         {
-#if DEBUG
+#if UNITY_EDITOR
             string template = ConsoleMessageTemplate(type, code, args);
-
-            Console.WriteLine(template.ToString());
+            
+            Debug.Log(template.ToString());
 #endif
         }
         else
         {
             // TODO: log 파일 출력
-#if DEBUG
+#if UNITY_EDITOR
             string template = ConsoleMessageTemplate(type, code, args);
 
-            Console.WriteLine(template.ToString());
+            Debug.Log(template.ToString());
 #endif
         }
     }
 
-    public override void Log(LogType type, LogCode code, params (LogKey key, object value)[] args)
+    public override void Log(CoreLibrary.Log.LogType type, LogCode code, params (LogKey key, object value)[] args)
     {
         if (code == LogCode.CONSOLE)
         {
-#if DEBUG
+#if UNITY_EDITOR
             string template = ConsoleMessageTemplate(type, code, args);
 
-            Console.WriteLine(template.ToString());
+            Debug.Log(template.ToString());
 #endif
         }
         else
         {
             // TODO: log 파일 출력
-#if DEBUG
+#if UNITY_EDITOR
             string template = ConsoleMessageTemplate(type, code, args);
 
-            Console.WriteLine(template.ToString());
+            Debug.Log(template.ToString());
 #endif
         }
     }
 
-    public override void Log(LogType type, LogCode code, params (string key, object value)[] args)
+    public override void Log(CoreLibrary.Log.LogType type, LogCode code, params (string key, object value)[] args)
     {
         if (code == LogCode.CONSOLE)
         {
-#if DEBUG
+#if UNITY_EDITOR
             string template = ConsoleMessageTemplate(type, code, args);
 
-            Console.WriteLine(template.ToString());
+            Debug.Log(template.ToString());
 #endif
         }
         else
         {
             // TODO: log 파일 출력
-#if DEBUG
+#if UNITY_EDITOR
             string template = ConsoleMessageTemplate(type, code, args);
 
-            Console.WriteLine(template.ToString());
+            Debug.Log(template.ToString());
 #endif
         }
     }
