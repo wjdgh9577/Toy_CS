e35aa15c6a4ebaeb288cd2a5d67972b23d9c4a43
Jungho Lee <wjdgh9577@naver.com>
2024-10-19 02:52:09 +0900

    CustomVector, Math 확장

--- Server/CoreLibrary/Utility/CustomVector2.cs
+++ Server/CoreLibrary/Utility/CustomVector2.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Server.Utility;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CoreLibrary.Utility
 {
-    public struct CustomVector2 : IEquatable<CustomVector2>, IEquatable<CustomVector2Int>
+    public struct CustomVector2 : IEquatable<CustomVector2>
     {
         public float x;
         public float y;
@@ -24,44 +25,53 @@ namespace CoreLibrary.Utility
         public bool Equals(CustomVector2 other)
             => this.x == other.x && this.y == other.y;
 
-        public bool Equals(CustomVector2Int other)
-            => this.x == other.x && this.y == other.y;
+        public static explicit operator CustomVector2Int(CustomVector2 a)
+            => new CustomVector2Int(a);
 
         public static bool operator ==(CustomVector2 a, CustomVector2 b)
-            => a.Equals(b);
-
-        public static bool operator ==(CustomVector2 a, CustomVector2Int b)
             => a.Equals(b);
 
         public static bool operator !=(CustomVector2 a, CustomVector2 b)
             => !a.Equals(b);
 
-        public static bool operator !=(CustomVector2 a, CustomVector2Int b)
-            => !a.Equals(b);
-
         public static CustomVector2 operator +(CustomVector2 a, CustomVector2 b)
-            => new CustomVector2(a.x + b.x, a.y + b.y);
-
-        public static CustomVector2 operator +(CustomVector2Int a, CustomVector2 b)
-            => new CustomVector2(a.x + b.x, a.y + b.y);
-
-        public static CustomVector2 operator +(CustomVector2 a, CustomVector2Int b)
             => new CustomVector2(a.x + b.x, a.y + b.y);
 
         public static CustomVector2 operator -(CustomVector2 a, CustomVector2 b)
             => new CustomVector2(a.x - b.x, a.y - b.y);
 
-        public static CustomVector2 operator -(CustomVector2Int a, CustomVector2 b)
-            => new CustomVector2(a.x - b.x, a.y - b.y);
+        public float Square()
+            => Square(this);
 
-        public static CustomVector2 operator -(CustomVector2 a, CustomVector2Int b)
-            => new CustomVector2(a.x - b.x, a.y - b.y);
+        public static float Square(CustomVector2 a)
+            => a.x * a.x + a.y * a.y;
 
-        public CustomVector2Int ToCustomVector2Int()
-            => new CustomVector2Int(this);
+        public float Distance(CustomVector2 a)
+            => Distance(this, a);
+
+        public static float Distance(CustomVector2 a, CustomVector2 b)
+            => (a - b).Magnitude();
+
+        public float Magnitude()
+            => Magnitude(this);
+
+        public static float Magnitude(CustomVector2 a)
+            => (float)Math.Sqrt(a.x * a.x + a.y * a.y);
+
+        public float Dot(CustomVector2 a)
+            => Dot(this, a);
+
+        public static float Dot(CustomVector2 a, CustomVector2 b)
+            => a.x * b.x + a.y * b.y;
+
+        public float Cross(CustomVector2 a)
+            => Cross(this, a);
+
+        public static float Cross(CustomVector2 a, CustomVector2 b)
+            => a.x * b.y - a.y * b.x;
     }
 
-    public struct CustomVector2Int : IEquatable<CustomVector2Int>, IEquatable<CustomVector2>
+    public struct CustomVector2Int : IEquatable<CustomVector2Int>
     {
         public int x;
         public int y;
@@ -74,32 +84,26 @@ namespace CoreLibrary.Utility
 
         public CustomVector2Int(float x, float y)
         {
-            this.x = (int)x;
-            this.y = (int)y;
+            this.x = x.ToInt32Safe();
+            this.y = y.ToInt32Safe();
         }
 
         public CustomVector2Int(CustomVector2 cv2)
         {
-            this.x = (int)cv2.x;
-            this.y = (int)cv2.y;
+            this.x = cv2.x.ToInt32Safe();
+            this.y = cv2.y.ToInt32Safe();
         }
 
         public bool Equals(CustomVector2Int other)
             => this.x == other.x && this.y == other.y;
 
-        public bool Equals(CustomVector2 other)
-            => this.x == other.x && this.y == other.y;
+        public static implicit operator CustomVector2(CustomVector2Int cv2i)
+            => new CustomVector2(cv2i);
 
         public static bool operator ==(CustomVector2Int a, CustomVector2Int b)
             => a.Equals(b);
 
-        public static bool operator ==(CustomVector2Int a, CustomVector2 b)
-            => a.Equals(b);
-
         public static bool operator !=(CustomVector2Int a, CustomVector2Int b)
-            => !a.Equals(b);
-
-        public static bool operator !=(CustomVector2Int a, CustomVector2 b)
             => !a.Equals(b);
 
         public static CustomVector2Int operator +(CustomVector2Int a, CustomVector2Int b)
@@ -107,8 +111,5 @@ namespace CoreLibrary.Utility
 
         public static CustomVector2Int operator -(CustomVector2Int a, CustomVector2Int b)
             => new CustomVector2Int(a.x - b.x, a.y - b.y);
-
-        public CustomVector2 ToCustomVector2()
-            => new CustomVector2(this);
     }
 }
---
+++ Server/CoreLibrary/Utility/MathExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Utility
+{
+    public static class MathExtension
+    {
+        public static float Epsilon = 1e-4f;
+
+        public static bool IsBetween(this float a, float b, float c, bool closed = true)
+        {
+            if (closed)
+                return a - Epsilon >= Math.Min(b, c) && a + Epsilon <= Math.Max(b, c);
+            else
+                return a - Epsilon > Math.Min(b, c) && a + Epsilon < Math.Max(b, c);
+        }
+
+        public static int ToInt32Safe(this float a)
+        {
+            var round = Convert.ToInt32(a);
+            var interval = Math.Abs(round - a);
+            return interval < Epsilon ? round : (int)a;
+        }
+    }
+}
